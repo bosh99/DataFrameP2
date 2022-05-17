665e1d35a0c8372a02803502fa61ba7ef310a7b9
estebanm30 <esteban.muriel30@gmail.com>
2022-05-17 17:15:21 -0500

    Esta listo el menu con todas las opciones del programa

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,28 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Esta clase se encarga de la ejejucion de la totalidad del codigo
+ * @author Esteban Muriel Roldan
+ * @author Miguel Vasquez Bojanini
+ * @author Manuel Villegas Michel
+ */
+
 public class Main {
+
+    /**
+     *  Se encarga de meter los datos del archivo en una matriz y se disponen las operacion para trabajar con ella
+     * @param args
+     */
+
     public static void main(String[] args) {
-        System.out.println("Datos climaticos de San Fransisco Downtown (2000 - 2019)");
-        System.out.println("Procesando... Por favor espere.");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Procesando... Por favor espere." + "\n");
         String[][] marcoDeDatos = new String[tamañoArchivo()][8];
         ArrayList<String[]> datos = new ArrayList<String[]>();
         lecturaDeDatos(datos);
         for(int i = 0; i < tamañoArchivo() ; i++) {
             Dato d1 = new Dato(datos.get(i));
-            //System.out.println(d1);
             marcoDeDatos[i][0] = d1.getEstacion();
             marcoDeDatos[i][1] = d1.getNombre();
             marcoDeDatos[i][2] = d1.getPais();
@@ -23,46 +35,132 @@ public class Main {
             marcoDeDatos[i][6] = d1.getTmax();
             marcoDeDatos[i][7] = d1.getTmin();
         }
-
-        /*
-        IMPRIMIR MATRIZ
-        imprimirMatriz(marcoDeDatos);
-        */
-
-        /*
-        PROMEDIOS
-        System.out.println("El promedio total de precipitaciones es: " + Operaciones.promedioPrcp(marcoDeDatos)+ "\n"
-                + "El promedio total de la temperatura promedio es: " + Operaciones.promedioTavg(marcoDeDatos)+"°C"+ "\n"
-                + "El promedio total de la temperatura maxima es: " + Operaciones.promedioTmax(marcoDeDatos)+"°C"+ "\n"
-                + "El promedio total de la temperatura minima es: " + Operaciones.promedioTmin(marcoDeDatos)+"°C");
-         */
-        /*
-        //NUMERO MAXIMOS
-        System.out.println("El mayor numero de precipitaciones es: " + Operaciones.mayorPrcp(marcoDeDatos)+ "\n"
-                + "La mayor cifra de temperatura promedio es: " + Operaciones.mayorTavg(marcoDeDatos)+"°C"+ "\n"
-                + "La mayor cifra de temperatura maxima es: " + Operaciones.mayorTmax(marcoDeDatos)+"°C"+ "\n"
-                + "La mayor cifra de temperatura minima es: " + Operaciones.mayorTmin(marcoDeDatos)+"°C");
-
-         */
-
-
-
-        /*
-        //NUMERO MINIMOS
-        System.out.println("El menor numero de precipitaciones es: " + Operaciones.menorPrcp(marcoDeDatos)+ "\n"
-                + "La menor cifra de temperatura promedio es: " + Operaciones.menorTavg(marcoDeDatos)+"°C"+ "\n"
-                + "La menor cifra de temperatura maxima es: " + Operaciones.menorTmax(marcoDeDatos)+"°C"+ "\n"
-                + "La menor cifra de temperatura minima es: " + Operaciones.menorTmin(marcoDeDatos)+"°C");
-
-         */
-
-
-
-
-
-
+        System.out.println("Bienvendio... Ingrese que desea hacer con los datos climaticos de San Fransisco Downtown (2000 - 2019)");
+        System.out.println("*Recuerde que al ser un archivo extenso, las operciones mas complicadas pueden tomar un tiempo*" + "\n");
+        Main.mostrarOpciones(marcoDeDatos);
     }
 
+    /**
+     * Imrpime el menu con las opciones del programa
+     */
+     public static void imprimirMenu(){
+       System.out.println("------------------------------------------------------------------------------");
+       System.out.println("| Digite [0] para finalizar el programa                                       |");
+       System.out.println("| Digite [1] para mostrar los datos en su totalidad                           |");
+       System.out.println("| Digite [2] para mostrar el promedio de las diferentes medidas climaticas    |");
+       System.out.println("| Digite [3] para mostrar los maximos de cada medida climatica                |");
+       System.out.println("| Digite [4] para mostrar los minimos de cada medida climatica                |");
+       System.out.println("| Digite [5] Para ordenar la lista de menor a mayor                           |");
+       System.out.println("| Digite [6] Para ordenar la lista de mayor a menor                           |");
+       System.out.println("------------------------------------------------------------------------------");
+     }
+
+    /**
+     * Se encarga de correr la opcion correspondiente de acuerdo con el numero que ingrese el usuario
+     * Implementa los metodos de la clase Operaciones
+     * @param marcoDeDatos es la matriz con los datos del archivo.csv
+     */
+        public static void mostrarOpciones (String [][] marcoDeDatos){
+                Scanner esc = new Scanner(System.in);
+
+                while (true){
+                    imprimirMenu();
+                    System.out.println();
+                    System.out.print("Digite la opcion seleccionada: ");
+                    int opcion = esc.nextInt();
+                    if (opcion != 0){
+                        switch (opcion){
+                            case 1:{
+                                imprimirMatriz(marcoDeDatos);
+                                break;
+                            }
+                            case 2:{
+                                System.out.println("Procesando... Por favor espere." + "\n");
+                                System.out.println("El promedio total de precipitaciones es: " + Operaciones.promedio(marcoDeDatos,4)+ "\n"
+                                 + "El promedio total de la temperatura promedio es: " + Operaciones.promedio(marcoDeDatos,5)+"°C"+ "\n"
+                                + "El promedio total de la temperatura maxima es: " + Operaciones.promedio(marcoDeDatos,6)+"°C"+ "\n"
+                                 + "El promedio total de la temperatura minima es: " + Operaciones.promedio(marcoDeDatos,7)+"°C");
+                                break;
+                            }
+                            case 3:{
+                                System.out.println("Procesando... Por favor espere." + "\n");
+                                System.out.println("El mayor numero de precipitaciones es: " + Operaciones.mayor(marcoDeDatos,4)+ "\n"
+                                + "La mayor cifra de temperatura promedio es: " + Operaciones.mayor(marcoDeDatos, 5)+"°C"+ "\n"
+                                + "La mayor cifra de temperatura maxima es: " + Operaciones.mayor(marcoDeDatos, 6)+"°C"+ "\n"
+                                + "La mayor cifra de temperatura minima es: " + Operaciones.mayor(marcoDeDatos, 7)+"°C");
+                                break;
+                            }
+                            case 4:{
+                                System.out.println("Procesando... Por favor espere." + "\n");
+                                System.out.println("El menor numero de precipitaciones es: " + Operaciones.menor(marcoDeDatos, 4)+ "\n"
+                                + "La menor cifra de temperatura promedio es: " + Operaciones.menor(marcoDeDatos, 5)+"°C"+ "\n"
+                                + "La menor cifra de temperatura maxima es: " + Operaciones.menor(marcoDeDatos, 6)+"°C"+ "\n"
+                                + "La menor cifra de temperatura minima es: " + Operaciones.menor(marcoDeDatos, 7)+"°C");
+                                break;
+                            }
+                            case 5:{
+                                System.out.println("Digite por que parametro lo quiere ordenar: ");
+                                System.out.println("[1]prcp [2]Tavg  [3]Tmax [4]Tmin ");
+                                opcion = esc.nextInt();
+                                System.out.println("Procesando... Por favor espere." + "\n");
+                                switch (opcion){
+                                    case 1:{
+                                        imprimirMatriz(Operaciones.ordenarMenorMayor(marcoDeDatos,4));
+                                        break;
+                                    }
+                                    case 2:{
+                                        imprimirMatriz(Operaciones.ordenarMenorMayor(marcoDeDatos,5));
+                                        break;
+                                    }
+                                    case 3:{
+                                        imprimirMatriz(Operaciones.ordenarMenorMayor(marcoDeDatos,6));
+                                        break;
+                                    }
+                                    case 4:{
+                                        imprimirMatriz(Operaciones.ordenarMenorMayor(marcoDeDatos,7));
+                                        break;
+                                    }
+                                }
+                                break;
+                            }
+                            case 6:{
+                                System.out.println("Digite por que parametro lo quiere ordenar: ");
+                                System.out.println("[1]prcp [2]Tavg  [3]Tmax [4]Tmin ");
+                                opcion = esc.nextInt();
+                                System.out.println("Procesando... Por favor espere." + "\n");
+                                switch (opcion){
+                                    case 1:{
+                                        imprimirMatriz(Operaciones.ordenarMayorMenor(marcoDeDatos,4));
+                                        break;
+                                    }
+                                    case 2:{
+                                        imprimirMatriz(Operaciones.ordenarMayorMenor(marcoDeDatos,5));
+                                        break;
+                                    }
+                                    case 3:{
+                                        imprimirMatriz(Operaciones.ordenarMayorMenor(marcoDeDatos,6));
+                                        break;
+                                    }
+                                    case 4:{
+                                        imprimirMatriz(Operaciones.ordenarMayorMenor(marcoDeDatos,7));
+                                        break;
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    else{
+                        System.out.println("Gracias por consultar... Adios");
+                        break;
+                    }
+                }
+            }
+
+    /**
+     *
+     * @param marcoDeDatos es la matriz con los datos del archivo.csv
+     */
     public static void imprimirMatriz(String[][] marcoDeDatos) {
         System.out.println("STATION " + " NAME "+" DATE "+" PRCP "+" TAVG "+" TMAX "+" TMIN");
         for(int i = 0; i < marcoDeDatos.length; i++) {
--- src/Operaciones.java
+++ src/Operaciones.java
@@ -3,124 +3,45 @@ import java.util.Arrays;
 
 public class Operaciones {
 
-    public static String promedioPrcp(String[][] marcoDeDatos) {
+
+    public static String promedio(String[][] marcoDeDatos, int columna){
         DecimalFormat formato = new DecimalFormat("#.##"); //Limitar la cantidad de decimales
         double total = 0;
         for(int i = 0; i < Main.tamañoArchivo(); i++) {
-            total += Double.parseDouble(marcoDeDatos[i][4]);
+            total += Double.parseDouble(marcoDeDatos[i][columna]);
         }
         return formato.format(total/Main.tamañoArchivo());
     }
 
-    public static String promedioTavg(String[][] marcoDeDatos) {
-        DecimalFormat formato = new DecimalFormat("#.##");
-        double total = 0;
-        for(int i = 0; i < Main.tamañoArchivo(); i++) {
-            total += Double.parseDouble(marcoDeDatos[i][5]);
-        }
-        return formato.format(total/Main.tamañoArchivo());
-    }
 
-    public static String promedioTmax(String[][] marcoDeDatos) {
-        DecimalFormat formato = new DecimalFormat("#.##");
-        double total = 0;
-        for(int i = 0; i < Main.tamañoArchivo(); i++) {
-            total += Double.parseDouble(marcoDeDatos[i][6]);
-        }
-        return formato.format(total/Main.tamañoArchivo());
-    }
 
-    public static String promedioTmin(String[][] marcoDeDatos) {
-        DecimalFormat formato = new DecimalFormat("#.##");
-        double total = 0;
-        for(int i = 0; i < Main.tamañoArchivo(); i++) {
-            total += Double.parseDouble(marcoDeDatos[i][7]);
-        }
-        return formato.format(total/Main.tamañoArchivo());
-    }
-
-    public static double mayorPrcp(String[][] marcoDeDatos){
+    public static double mayor(String[][] marcoDeDatos, int columna){
         double mayor = 0;
         for (int i = 0; i < Main.tamañoArchivo(); i++){
-            if (Double.parseDouble(marcoDeDatos[i][4]) > mayor){
-                mayor = Double.parseDouble(marcoDeDatos[i][4]);
+            if (Double.parseDouble(marcoDeDatos[i][columna]) > mayor){
+                mayor = Double.parseDouble(marcoDeDatos[i][columna]);
             }
         }
         return mayor;
     }
 
-    public static double mayorTavg(String[][] marcoDeDatos){
-        double mayor = 0;
-        for (int i = 0; i < Main.tamañoArchivo(); i++){
-            if (Double.parseDouble(marcoDeDatos[i][5]) > mayor){
-                mayor = Double.parseDouble(marcoDeDatos[i][5]);
-            }
-        }
-        return mayor;
-    }
-
-    public static double mayorTmax(String[][] marcoDeDatos){
-        double mayor = 0;
-        for (int i = 0; i < Main.tamañoArchivo(); i++){
-            if (Double.parseDouble(marcoDeDatos[i][6]) > mayor){
-                mayor = Double.parseDouble(marcoDeDatos[i][6]);
-            }
-        }
-        return mayor;
-    }
-
-    public static double mayorTmin(String[][] marcoDeDatos){
-        double mayor = 0;
-        for (int i = 0; i < Main.tamañoArchivo(); i++){
-            if (Double.parseDouble(marcoDeDatos[i][7]) > mayor){
-                mayor = Double.parseDouble(marcoDeDatos[i][7]);
-            }
-        }
-        return mayor;
-    }
 
     // Los minimos serian todos 0.0, entonces no se si poner que sea diferente a 0.0
 
-    public static double menorPrcp(String[][] marcoDeDatos){
+    public static double menor(String[][] marcoDeDatos, int columnas){
         double menor = Double.MAX_VALUE;
         for (int i = 0; i < Main.tamañoArchivo(); i++){
-            if (Double.parseDouble(marcoDeDatos[i][4]) < menor && Double.parseDouble(marcoDeDatos[i][4]) != 0.0){
-                menor = Double.parseDouble(marcoDeDatos[i][4]);
-            }
-        }
-        return menor;
-    }
-    public static double menorTavg(String[][] marcoDeDatos){
-        double menor = Double.MAX_VALUE;
-        for (int i = 0; i < Main.tamañoArchivo(); i++){
-            if (Double.parseDouble(marcoDeDatos[i][5]) < menor && Double.parseDouble(marcoDeDatos[i][5]) != 0.0){
-                menor = Double.parseDouble(marcoDeDatos[i][5]);
+            if (Double.parseDouble(marcoDeDatos[i][columnas]) < menor && Double.parseDouble(marcoDeDatos[i][columnas]) != 0.0){
+                menor = Double.parseDouble(marcoDeDatos[i][columnas]);
             }
         }
         return menor;
     }
 
-    public static double menorTmax(String[][] marcoDeDatos){
-        double menor = Double.MAX_VALUE;
-        for (int i = 0; i < Main.tamañoArchivo(); i++){
-            if (Double.parseDouble(marcoDeDatos[i][6]) < menor && Double.parseDouble(marcoDeDatos[i][6]) != 0.0){
-                menor = Double.parseDouble(marcoDeDatos[i][6]);
-            }
-        }
-        return menor;
-    }
 
-    public static double menorTmin(String[][] marcoDeDatos){
-        double menor = Double.MAX_VALUE;
-        for (int i = 0; i < Main.tamañoArchivo(); i++){
-            if (Double.parseDouble(marcoDeDatos[i][7]) < menor && Double.parseDouble(marcoDeDatos[i][7]) != 0.0){
-                menor = Double.parseDouble(marcoDeDatos[i][7]);
-            }
-        }
-        return menor;
-    }
 /*
 // La moda de demora 3 dias en ejecutarse
+
     public static void modaPrcp(String[][] marcoDeDatos) {
         int maximoNumRepeticiones = 0;
         String moda = "";
@@ -142,10 +63,35 @@ public class Operaciones {
 
  */
 
-    public static String[][] ordenarMayorMenorPrcp(String[][] marcoDeDatos){
 
+    public static String[][] ordenarMenorMayor(String[][] marcoDeDatos, int orderBy){
+        String[] temp = new String[7];
+        for(int i = 0; i < Main.tamañoArchivo(); i++){
+            int pos = i;
+            temp = marcoDeDatos[i];
+            while((pos > 0) && (Double.parseDouble(marcoDeDatos[pos-1][orderBy]) > Double.parseDouble(temp[orderBy]))){
+                marcoDeDatos[pos] = marcoDeDatos[pos-1];
+                pos--;
+            }
+            marcoDeDatos[pos] = temp;
+
+        }
+        return marcoDeDatos;
     }
 
+    public static String[][] ordenarMayorMenor(String[][] marcoDeDatos, int orderBy){
+        String[] temp = new String[7];
+        for(int i = 0; i < Main.tamañoArchivo(); i++){
+            int pos = i;
+            temp = marcoDeDatos[i];
+            while((pos > 0) && (Double.parseDouble(marcoDeDatos[pos-1][orderBy]) < Double.parseDouble(temp[orderBy]))){
+                marcoDeDatos[pos] = marcoDeDatos[pos-1];
+                pos--;
+            }
+            marcoDeDatos[pos] = temp;
 
+        }
+        return marcoDeDatos;
+    }
 
 }
